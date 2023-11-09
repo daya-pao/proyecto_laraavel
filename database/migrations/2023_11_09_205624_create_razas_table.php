24c312501cdc6c62d83109461bc1b1a07bd69e25
daya-pao <dayana06052002@gmail.com>
2023-11-09 16:38:36 -0500

    migracion database

---
+++ database/migrations/2023_11_09_205624_create_razas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\TipoMascota;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use PHPUnit\Framework\Constraint\Constraint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('razas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre' ,100);
+            $table->foreignIdFor(TipoMascota::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('razas');
+    }
+};
---
+++ database/migrations/2023_11_09_210417_create_mascotas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use App\Models\Raza;
+use App\Models\TipoMascota;
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mascotas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre',50);
+            $table->date('FechaNacimiento');
+            $table->string('foto',250);
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(TipoMascota::class)->constrained();
+            $table->foreignIdFor(Raza::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mascotas');
+    }
+};
